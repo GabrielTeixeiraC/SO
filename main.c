094a86590a62823af40437f93f5d8100146a110d
Gabriel Teixeira <Gteixeiraca@gmail.com>
2023-05-02 21:05:31 -0300

    Feat: Sample dlist use

--- main.c
+++ main.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dlist.h"
 
 int main(int argc, char *argv[])
 {
-  struct dlist *dl = dlist_create();
-  printf("dl->count = %d\n", dl->count);
-  return 0;
+    struct dlist *dl = dlist_create();
+
+    int *a = malloc(sizeof(int));
+    int *b = malloc(sizeof(int));
+    int *c = malloc(sizeof(int));
+
+    *a = 10;
+    *b = 20;
+    *c = 30;
+
+    dlist_push_right(dl, a);
+    dlist_push_right(dl, b);
+    dlist_push_right(dl, c);
+
+    struct dnode *node = dl->head;
+    while (node != NULL) {
+        printf("%d\n", *(int *)node->data);
+        node = node->next;
+    }
+
+    dlist_destroy(dl, free);
+    return 0;
 }
