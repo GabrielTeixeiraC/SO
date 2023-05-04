64ed84cb12f45ab29fbf66394bc92686483bedd2
Gabriel Teixeira <Gteixeiraca@gmail.com>
2023-05-04 04:08:06 -0300

    Feat: Creating and executing Threads Complete

--- dccthread.c
+++ dccthread.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ucontext.h>
@@ -8,18 +9,37 @@
 #define MAX_THREAD_NAME_SIZE 128
 
 struct dccthread {
-	ucontext_t context;
 	char name[MAX_THREAD_NAME_SIZE];
+	ucontext_t context;
+	int yielded;
+	int waiting;
 };
 
 ucontext_t manager;
+struct dlist *threads;
 
 void dccthread_init(void (*func)(int), int param) {
-	dccthread_t *thread = dccthread_create("main", func, param);
+	threads = dlist_create();
+	dccthread_create("main", func, param);
 	getcontext(&manager);
 
-	swapcontext(&manager, &thread->context);
+	while (!dlist_empty(threads)) {
+		dccthread_t *thread = dlist_get_index(threads, 0);
 
+		if (thread->waiting) {
+			dlist_pop_left(threads);
+			dlist_push_right(threads, thread);
+			continue;
+		}
+
+		swapcontext(&manager, &thread->context);
+		dlist_pop_left(threads);
+
+		if (thread->yielded || thread->waiting) {
+			thread->yielded = 0;
+			dlist_push_right(threads, thread);
+		}
+	}
 
 	exit(0);
 }
@@ -28,6 +48,8 @@ dccthread_t * dccthread_create(const char *name, void (*func)(int ), int param) 
 	dccthread_t *thread = malloc(sizeof(dccthread_t));
 	
 	strcpy(thread->name, name);
+	thread->yielded = 0;
+	thread->waiting = 0;
 	
 	ucontext_t context;
 	getcontext(&context);
@@ -37,27 +59,34 @@ dccthread_t * dccthread_create(const char *name, void (*func)(int ), int param) 
 
 	thread->context = context;
 
+	dlist_push_right(threads, thread);
+
 	makecontext(&thread->context, (void (*)()) func, 1, param);
 
 	return thread;
 
 }
+
 void dccthread_yield(void) {
-	// TODO
+	dccthread_t *current_thread = dccthread_self();
+	current_thread->yielded = 1;
+	swapcontext(&current_thread->context, &manager);
 }
+
 void dccthread_exit(void) {
-	// TODO
 }
+
 void dccthread_wait(dccthread_t *tid) {
-	// TODO
 }
+
 void dccthread_sleep(struct timespec ts) {
 	// TODO
 }
+
 dccthread_t * dccthread_self(void) {
-	// TODO
-	return NULL;
+	return dlist_get_index(threads, 0);
 }
+
 const char * dccthread_name(dccthread_t *tid) {
 	return tid->name;
 }
