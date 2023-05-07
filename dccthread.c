659394635690a0b3fe9ef19a936edd6222e1fd26
Gabriel Teixeira <Gteixeiraca@gmail.com>
2023-05-07 12:37:46 -0300

    Feat: Up to test 10 working

--- dccthread.c
+++ dccthread.c
@@ -11,38 +11,45 @@
 #define STACK_SIZE 1024*1024
 #define MAX_THREAD_NAME_SIZE 128
 #define TIMER_INTERVAL_SEC 0
-#define TIMER_INTERVAL_NSEC 100000000
+#define TIMER_INTERVAL_NSEC 10000000
 
 struct dccthread {
 	char name[MAX_THREAD_NAME_SIZE];
 	ucontext_t * context;
 	int yielded;
-	int sleeping;
 	dccthread_t * waited_thread;
 };
 
 ucontext_t manager;
 struct dlist * threads;
-int timer_enabled = 0;
+struct dlist * sleeping_threads;
+sigset_t mask;
 
-void disable_timer() {
-	timer_enabled = 0;
-	sigset_t mask;
-	sigemptyset(&mask);
-	sigaddset(&mask, SIGALRM);
-	sigprocmask(SIG_BLOCK, &mask, NULL);
-}
+dccthread_t * current_thread;
 
-void enable_timer() {
-	timer_enabled = 1;
-	sigset_t mask;
-	sigemptyset(&mask);
-	sigaddset(&mask, SIGALRM);
-	sigprocmask(SIG_UNBLOCK, &mask, NULL);
+int find_thread_in_lists(dccthread_t * thread) {
+	struct dnode * node = threads->head;
+	while (node != NULL) {
+		if (node->data == thread) {
+			return 1;
+		}
+		node = node->next;
+	}
+
+	node = sleeping_threads->head;
+	while (node != NULL) {
+		if (node->data == thread) {
+			return 1;
+		}
+		node = node->next;
+	}
+
+	return 0;
 }
 
 void dccthread_init(void (*func)(int), int param) {
 	threads = dlist_create();
+	sleeping_threads = dlist_create();
 	dccthread_create("main", func, param);
 	getcontext(&manager);
 
@@ -63,36 +70,38 @@ void dccthread_init(void (*func)(int), int param) {
   ts.it_interval.tv_nsec = TIMER_INTERVAL_NSEC;
   ts.it_value.tv_sec = TIMER_INTERVAL_SEC;
   ts.it_value.tv_nsec = TIMER_INTERVAL_NSEC;
+  
+	sigaction(SIGRTMIN, &sa, NULL);
 
-  sigaction(SIGRTMIN, &sa, NULL);  
+	sigemptyset(&mask);
+	sigaddset(&mask, SIGRTMIN);
+	sigprocmask(SIG_BLOCK, &mask, NULL);
 
-	disable_timer();
+	timer_create(CLOCK_PROCESS_CPUTIME_ID, &se, &timer_id);
 
-  if (timer_create(CLOCK_PROCESS_CPUTIME_ID, &se, &timer_id) == -1) {
-    perror("Error creating timer");
-    exit(EXIT_FAILURE);
-  }
+	timer_settime(timer_id, 0, &ts, NULL);
 
-  if (timer_settime(timer_id, 0, &ts, NULL) == -1) {
-    perror("Error setting timer");
-    exit(EXIT_FAILURE);
-  }
-
-	while (!dlist_empty(threads)) {
-		dccthread_t *thread = dlist_get_index(threads, 0);
-
-		if (thread->waited_thread != NULL || thread->sleeping) {
+	while (!dlist_empty(threads) || !dlist_empty(sleeping_threads)) {
+		dccthread_t * current_thread = dlist_get_index(threads, 0);
+		if (dlist_empty(threads)) {
+			current_thread = dlist_get_index(sleeping_threads, 0);
+		}
+		printf("threads: %d, sleeping_threads: %d, thread %s running\n", threads->count, sleeping_threads->count, current_thread->name);
+		
+		if (current_thread->waited_thread != NULL) {
+			dlist_push_right(threads, current_thread);
 			dlist_pop_left(threads);
-			dlist_push_right(threads, thread);
 			continue;
 		}
 
-		swapcontext(&manager, thread->context);
+		// printf("threads: %d, sleeping_threads: %d, thread %s running\n", threads->count, sleeping_threads->count, current_thread->name);
+		swapcontext(&manager, current_thread->context);
+		
 		dlist_pop_left(threads);
 
-		if (thread->yielded || thread->waited_thread != NULL) {
-			thread->yielded = 0;
-			dlist_push_right(threads, thread);
+		if (current_thread->yielded || current_thread->waited_thread != NULL) {
+			current_thread->yielded = 0;
+			dlist_push_right(threads, current_thread);
 		}
 	}
 
@@ -101,14 +110,17 @@ void dccthread_init(void (*func)(int), int param) {
     exit(EXIT_FAILURE);
   }
 
+	dlist_destroy(threads, NULL);
+	dlist_destroy(sleeping_threads, NULL);
+
 	exit(0);
 }
 
 dccthread_t * dccthread_create(const char *name, void (*func)(int ), int param) {
-	disable_timer();
-	
 	ucontext_t * context = malloc(sizeof(ucontext_t));
 	getcontext(context);
+
+	sigprocmask(SIG_BLOCK, &mask, NULL);
 	context->uc_link = &manager;
 	context->uc_stack.ss_sp = malloc(STACK_SIZE);
 	context->uc_stack.ss_size = STACK_SIZE;
@@ -118,72 +130,129 @@ dccthread_t * dccthread_create(const char *name, void (*func)(int ), int param) 
 	strcpy(thread->name, name);
 	thread->context = context;
 	thread->yielded = 0;
-	thread->sleeping = 0;
 	thread->waited_thread = NULL;
 
 	dlist_push_right(threads, thread);
 
 	makecontext(thread->context, (void (*)()) func, 1, param);
 
-	enable_timer();
+	sigprocmask(SIG_UNBLOCK, &mask, NULL);
 
 	return thread;
 }
 
 void dccthread_yield(void) {
-	disable_timer();
-
-	dccthread_t *current_thread = dccthread_self();
+	sigprocmask(SIG_BLOCK, &mask, NULL);
+	dccthread_t * current_thread = dlist_get_index(threads, 0);
 	current_thread->yielded = 1;
-
-	enable_timer();
+	// printf("threads: %d, sleeping_threads: %d, thread %s yielding to manager\n", threads->count, sleeping_threads->count, current_thread->name);
 	swapcontext(current_thread->context, &manager);
+	sigprocmask(SIG_UNBLOCK, &mask, NULL);
 }
 
 void dccthread_exit(void) {
-	disable_timer();
+	sigprocmask(SIG_BLOCK, &mask, NULL);
+	dccthread_t * current_thread = dlist_get_index(threads, 0);
+	// printf("threads: %d, sleeping_threads: %d, thread %s exiting\n", threads->count - 1, sleeping_threads->count, current_thread->name);
 
-	dccthread_t *current_thread = dccthread_self();
-	struct dnode *node = threads->head;
-
-	while (node != NULL) {
-		dccthread_t *thread = node->data;
+	for (int i = 0; i < threads->count; i++) {
+		// printf("i: %d\n", i);
+		dccthread_t *thread = dlist_get_index(threads, i);
 		if (thread->waited_thread == current_thread) {
-			thread->waited_thread = NULL;
+			printf("threads: %d, sleeping_threads: %d, thread %s waking up\n", threads->count - 1, sleeping_threads->count, thread->name);
+			// thread->waited_thread = NULL;
 		}
-		node = node->next;
+	}
+	
+	// printf("threads: %d, sleeping_threads: %d, thread %s freeing\n", threads->count - 1, sleeping_threads->count, current_thread->name);
+	for (int i = 0; i < sleeping_threads->count; i++) {
+		dccthread_t *thread = dlist_get_index(sleeping_threads, i);
+		if (thread->waited_thread == current_thread) {
+			// thread->waited_thread = NULL;
+		}
 	}
 	
 	free(current_thread);
-
-	enable_timer();
+	// printf("freed!\n");
+	swapcontext(current_thread->context, &manager);
+	sigprocmask(SIG_UNBLOCK, &mask, NULL);
 }
 
 void dccthread_wait(dccthread_t *tid) {
-	disable_timer();
+	sigprocmask(SIG_BLOCK, &mask, NULL);
 
-	struct dnode *node = threads->head;
-
-	while (node != NULL) {
-		dccthread_t *thread = node->data;
-		if (thread == tid) {
-			break;
-		}
-		node = node->next;
-	}
-
-	if (node == NULL) {
+	if (!find_thread_in_lists(tid)) {
 		return;
 	}
 
-	dccthread_t *current_thread = dccthread_self();
+	dccthread_t * current_thread = dlist_get_index(threads, 0);
+
 	current_thread->waited_thread = tid;
 
-	enable_timer();
+	// printf("threads: %d, sleeping_threads: %d, thread %s waiting for thread %s\n", threads->count, sleeping_threads->count, current_thread->name, tid->name);
 	swapcontext(current_thread->context, &manager);
+	sigprocmask(SIG_UNBLOCK, &mask, NULL);
+}
+
+int cmp (const void * a, const void * b, void * param) {
+	dccthread_t *thread_a = (dccthread_t *) a;
+	dccthread_t *thread_b = (dccthread_t *) b;
+
+	return (thread_a != thread_b);
+}
+
+void dccthread_wakeup() {
+	sigprocmask(SIG_BLOCK, &mask, NULL);
+
+	// struct dnode *node = sleeping_threads->head; 
+	// while (node != NULL) {
+	// 	dccthread_t *thread = node->data;
+	// 	if (thread == (dccthread_t *)si->si_value.sival_ptr){
+	// 		dlist_find_remove(sleeping_threads, node, cmp, NULL);
+	// 		dlist_push_right(threads, thread);
+	// 		break;
+	// 	}
+	// 	node = node->next;
+	// }
+	dccthread_t *current_thread = dlist_get_index(sleeping_threads, 0);
+	// printf("threads: %d, sleeping_threads: %d, thread %s waking up\n", threads->count, sleeping_threads->count, thread->name);
+	dlist_pop_left(sleeping_threads);
+	dlist_push_right(threads, current_thread);
+
+	// printf("threads: %d, sleeping_threads: %d, thread %s woke up\n", threads->count, sleeping_threads->count, thread->name);
+	sigprocmask(SIG_UNBLOCK, &mask, NULL);
 }
 
 void dccthread_sleep(struct timespec ts) {
+	sigprocmask(SIG_BLOCK, &mask, NULL);
+
+	dccthread_t * current_thread = dlist_get_index(threads, 0);
+
+	timer_t st;
+	struct sigevent sse;
+	struct sigaction ssa;
+	struct itimerspec sts;
+
+	ssa.sa_flags = SA_SIGINFO;
+	ssa.sa_sigaction = dccthread_wakeup;
+	ssa.sa_mask = mask;
+	sigaction(SIGRTMAX, &ssa, NULL);
+
+	sse.sigev_notify = SIGEV_SIGNAL;
+	sse.sigev_signo = SIGRTMAX;
+	sse.sigev_value.sival_ptr = current_thread;
+	timer_create(CLOCK_REALTIME, &sse, &st);
+
+	sts.it_value = ts;
+	sts.it_interval.tv_sec = 0;
+	sts.it_interval.tv_nsec = 0;
+	timer_settime(st, 0, &sts, NULL);
+
+	dlist_push_right(sleeping_threads, current_thread);
+
+	// printf("threads: %d, sleeping_threads: %d, thread %s sleeping\n", threads->count, sleeping_threads->count, current_thread->name);
+	swapcontext(current_thread->context, &manager);
+	sigprocmask(SIG_UNBLOCK, &mask, NULL);
 }
 
 dccthread_t * dccthread_self(void) {
